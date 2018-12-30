b8fe8b13ed8dbd9f7d430236fb1bd6899be4098a
Allen Wild <allenwild93@gmail.com>
2018-12-29 21:25:37 -0500

    main: hook up logfile and syslog command line flags

--- main.go
+++ main.go
@@ -15,17 +15,30 @@ var opts struct{
     showVersion bool
     debug       bool
     logLevel    int
+    logFile     string
+    logStderr   bool
+    syslog      bool
+    syslogFac   int
+    syslogTag   string
     listenAddr  string
     sshDir      string
 }
 
 func main() {
+    // main options
     flag.BoolVar(&opts.showVersion, "V", false, "Show version and exit")
-    flag.BoolVar(&opts.debug, "D", false, "Enable debug logging (same as -loglevel=4)")
     flag.StringVar(&opts.listenAddr, "port", "2222", "Listen address/port, format [host:]port")
     flag.StringVar(&opts.sshDir, "sshdir", "ssh", "Directory containing SSH host keys and authorized_keys")
+
+    // log options
+    flag.BoolVar(&opts.debug, "D", false, "Enable debug logging (same as -loglevel=4)")
     flag.IntVar(&opts.logLevel, "loglevel", int(LOG_LEVEL_INFO),
                 "Log level number: 0=fatal, 1=error, 2=warn, 3=info (default), 4=debug")
+    flag.StringVar(&opts.logFile, "logfile", "", "Log file name (default none)")
+    flag.BoolVar(&opts.logStderr, "E", false, "Log to stderr (default on unless -syslog or -logfile are given)")
+    flag.BoolVar(&opts.syslog, "syslog", false, "Log to syslog (default off)")
+    flag.IntVar(&opts.syslogFac, "facility", 18, "syslog facility code (default 18/local2)")
+    flag.StringVar(&opts.syslogTag, "tag", "wolssh", "syslog message tag")
 
     flag.Parse()
 
@@ -34,12 +47,21 @@ func main() {
         os.Exit(0)
     }
 
+    // logging setup
     level := LogLevel(opts.logLevel)
     if opts.debug {
         level = LOG_LEVEL_DEBUG
     }
-    //log = NewLogger(level, true, "test.log", &SyslogConfig{facility:18, tag:"wolssh"})
-    log = NewLogger(level, true, "", nil)
+
+    var syslogConfig *SyslogConfig = nil
+    if opts.syslog {
+        syslogConfig = &SyslogConfig{facility:opts.syslogFac, tag:opts.syslogTag}
+    } else if opts.logFile == "" {
+        // force enable stderr logging if no file or syslog given
+        opts.logStderr = true
+    }
+
+    log = NewLogger(level, opts.logStderr, opts.logFile, syslogConfig)
 
     if !strings.Contains(opts.listenAddr, ":") {
         opts.listenAddr = ":" + opts.listenAddr
